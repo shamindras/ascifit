e9f27cc1f4c591526d429f6ff5249f28ba301a16
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2022-01-27 23:30:32 -0500

    ENH: #3 Get simulation plots setup

---
+++ experiments/R/create-ascifit-data-02-plot.R
@@ -0,0 +1,130 @@
+# Load libraries
+# devtools::load_all()
+# devtools::document()
+library(tidyverse)
+library(here)
+library(scales)
+library(ggthemes)
+
+mse <- function(estimate, actual){
+  base::return(mean((estimate - actual)**2))
+}
+
+# Set seed for reproducibility
+set.seed(523525)
+
+# Read out_ascifit1 data
+# TODO: Change to the correct file for final check
+out_ascifit1_r <- readr::read_rds(file = here::here("experiments", "R", "out_ascifit1_50_reps.rds"))
+# out_ascifit1_r <- readr::read_rds(file = here::here("experiments", "R", "out_ascifit1.rds"))
+
+# Produce plots for conference paper
+out_ascifit2 <- out_ascifit1_r %>%
+  dplyr::mutate(mu_hat_inv = purrr::map(.x = .data[["ascifit_out"]], .f = ~.x$mu_hat_inv),
+                sigma_hat = purrr::map(.x = .data[["ascifit_out"]], .f = ~.x$sigma_hat),
+                mu_hat = purrr::map(.x = .data[["ascifit_out"]], .f = ~.x$mu_hat),
+                mu = purrr::map(.x = .data[["asci_data_out"]], .f = ~.x$mu_i),
+                mse = purrr::map2_dbl(.x = .data[["mu_hat"]], .y = .data[["mu"]],
+                                      .f = ~mse(estimate = .x, actual = .y)))
+
+# Plot mean-MSE of ASCIFIT over grid --------------------------------------
+# Wrangle the plotting data
+# For a fixed value of eta, p, summarize the mean-MSE by n and sigma
+out_ascifit2_mse <- out_ascifit2 %>%
+  dplyr::select(n, eta, p, sigma, mse) %>%
+  # dplyr::filter(eta == 1/5, p == 0.25, sigma == 1) %>%
+  dplyr::filter(eta == 1/5, p == 0.5) %>%
+  group_by(n, sigma) %>%
+  dplyr::summarize(mean_mse = base::mean(mse),
+                   sd_mse = stats::sd(mse)) %>%
+  dplyr::mutate(se_mse = sd_mse/base::sqrt(n),
+                sigma = as.factor(sigma),
+                n = as.factor(n))
+
+# For a fixed value of eta, p, plot the mean-MSE by n and sigma
+
+# The errorbars overlapped, so use position_dodge to move them horizontally
+pd <- ggplot2::position_dodge(0.1) # move them .05 to the left and right
+
+out_ascifit2_mse_plt <- out_ascifit2_mse %>%
+  ggplot2::ggplot(data = ., ggplot2::aes(x = n, y = mean_mse, color = sigma,
+                                         fill = sigma)) +
+  ggplot2::geom_errorbar(ggplot2::aes(ymin = mean_mse - 2*se_mse,
+                                      ymax = mean_mse + 2*se_mse), width = .5,
+                         position = pd,
+                         size = 2) +
+  ggplot2::geom_line(position=pd) +
+  ggplot2::geom_point(position=pd, size=4, shape=21) + # 21 is filled circle
+  ggplot2::labs(title = latex2exp::TeX("Sample mean-MSE for estimating $\\mu$ using ASCIFIT"),
+                x = latex2exp::TeX("Sample size $(n)$"),
+                y = latex2exp::TeX("mean-MSE $\\frac{1}{n} \\| \\hat{\\mu} - mu \\|^{2}$")) +
+  ggplot2::scale_y_continuous(breaks=c(0, 0.2, 0.4, 0.6, 0.8, 1)) +
+  # ggthemes::theme_economist() +
+  ggthemes::theme_igray() +
+  ggplot2::theme(legend.position="bottom",
+                 text = element_text(size=25))
+
+# Inspect plot
+out_ascifit2_mse_plt
+
+# Save plot
+cowplot::ggsave2(filename = here::here("experiments", "R",
+                                       "out_ascifit1_50_reps.png"),
+                 plot = out_ascifit2_mse_plt,
+                 # width = 10,
+                 # height = 50,
+                 dpi = 900)
+
+# Plot the simulated results ---------------------------------------------------
+plt_eta <- 0.2
+plt_p <- 0.5
+plt_sigma <- 2
+plt_reps <- 40
+plt_n <- 1000
+out_ascifit3 <- out_ascifit2 %>%
+  dplyr::filter(eta == plt_eta, p == plt_p, sigma == plt_sigma,
+                reps == plt_reps, n == plt_n)
+out_ascifit3
+
+out_ascifit3 %>%
+  dplyr::pull(ascifit_comb) %>%
+  .[[1]]
+
+sim_plt <- out_ascifit3 %>%
+  dplyr::pull(ascifit_comb) %>%
+  .[[1]] %>%
+  dplyr::select(-eps_i, -mu_hat_inv_i) %>%
+  tidyr::pivot_longer(data = .,
+                      cols = -c("x_i", "xi_i", "t_i"),
+                      names_to = c("y_type")) %>%
+  dplyr::mutate(y_type = base::as.factor(y_type)) %>%
+  ggplot2::ggplot(data = .,
+                  mapping = ggplot2::aes(x = x_i, y = value)) +
+  ggplot2::geom_point(ggplot2::aes(color = y_type)) +
+  ggplot2::ylim(-5, 5) +
+  ggplot2::xlim(0, plt_n) +
+  # ggthemes::theme_economist() +
+  ggthemes::theme_igray() +
+  ggplot2::scale_color_hue(labels = c(latex2exp::TeX("$\\hat{\\mu}$"),
+                                      latex2exp::TeX("$\\mu$"),
+                                      latex2exp::TeX("$R_{i}$"),
+                                      latex2exp::TeX("$Y_{i}$"))) +
+  ggplot2::labs(
+    title = "Plot of true values, response, and ASCIFIT",
+    x = "x",
+    y = "response"
+    # y = "true values, response, and ASCIFIT"
+  ) +
+  # ggplot2::theme_minimal() +
+  ggplot2::theme(legend.position="bottom",
+                 text = element_text(size=20),
+                 legend.title = ggplot2::element_blank())
+sim_plt
+
+# Save plot
+cowplot::ggsave2(filename = here::here("experiments", "R",
+                                       "sim_plt_50_reps.png"),
+                 plot = sim_plt,
+                 # width = 10,
+                 # height = 50,
+                 dpi = 900)
--- experiments/R/create-ascifit-data-02.R
+++ experiments/R/create-ascifit-data-02.R
@@ -1,8 +1,10 @@
 # Load libraries
 # devtools::load_all()
 # devtools::document()
-# library(fAsianOptions) # needed for the fAsianOptions::erf function
+library(fAsianOptions) # needed for the fAsianOptions::erf function
 library(tidyverse)
+library(here)
+library(scales)
 
 inverse_mean_sigma <- function (f, sigma, lower = 0, upper = 10000) {
   function (y) uniroot((function (x) f(x, sigma) - y), lower = lower, upper = upper)[1]
@@ -51,25 +53,30 @@ ascifit <- function(R_vals, eta) {
 }
 
 # Wrapper - generate ASCI data --------------------------------------------
-gen_asci_data_type1 <- function(n, p, eta, sigma){
+gen_asci_data_type1 <- function(tot_sims, sim_no, n, p, eta, sigma){
+  print(glue::glue("Simulation {sim_no} of {tot_sims}: Progress {scales::percent(sim_no/tot_sims)}\\n"))
   # Rademacher(p) contamination
-  xi <- (2 * rbinom(n = n, size = 1, prob = p) - 1)
+  xi <- 2 * rbinom(n = n, size = 1, prob = p) - 1
 
   # True response
   mu <- seq(from = n * eta, to = n, length.out = n) / n
   eps <- rnorm(n = n, mean = 0, sd = sigma)
 
   # Contaminated response
-  R <- xi * mu + eps
+  # R <- xi * mu + eps
+  y <- mu + eps
+  R <- xi * y
+  T_vals <- base::abs(R)
 
   # ASCI generated data
   asci_gen_data <- tibble::tibble(
-    mu_i_eta = mu,
+    x_i = seq(from = 1, to = n, length.out = n),
+    mu_i = mu,
     eps_i = eps,
     xi_i = xi,
-    y_i = mu + eps,
+    y_i = y,
     r_i = R,
-    t_i = base::abs(R)
+    t_i = T_vals
   )
 
   # # Load the ASCI generated data, and eta values into a list
@@ -84,17 +91,25 @@ ascifit_wrapper <- function(gen_asci_data, eta){
   base::return(out_list)
 }
 
-mse <- function(estimate, actual){
-  base::return(mean((estimate - actual)**2))
+asci_data_wrapper <- function(asci_data_out, ascifit_out){
+  out_comb_data <- asci_data_out %>%
+    dplyr::mutate(mu_hat_inv_i = ascifit_out$mu_hat_inv,
+                  mu_hat_i = ascifit_out$mu_hat)
+  base::return(out_comb_data)
 }
+
+# Set seed for reproducibility
+set.seed(4265436)
 
 # Generate ASCI grid -------------------------------------------------------------
 # Define grid parameter value ranges
-exp_n <- base::seq(from = 50, to = 200, by = 50)
-exp_sigma <- base::seq(from = 0.5, to = 1, by = 0.5)
+exp_n <- c(100L, 250L, 500L, 1000L)
+exp_sigma <- base::seq(from = 0.5, to = 3, by = 0.5)
 exp_eta <- 1/5
-exp_p <- base::seq(from = 0.25, to = 0.75, by = 0.5)
-exp_reps <- 1:20
+exp_p <- 0.5
+exp_reps <- 1:100
+# Get total number of sims. Useful for printing
+tot_sims <- length(exp_n) * length(exp_sigma) * length(exp_eta) * length(exp_p) * length(exp_reps)
 
 # Generate the ASCI parameter grid
 asci_grid <- tidyr::crossing(n = exp_n,
@@ -102,65 +117,32 @@ asci_grid <- tidyr::crossing(n = exp_n,
                              eta = exp_eta,
                              p = exp_p,
                              reps = exp_reps) %>%
-  dplyr::mutate(sim_no = dplyr::row_number()) %>%
-  dplyr::relocate(.data = ., sim_no, reps)
+  # Add in row number index for the replications
+  dplyr::mutate(sim_no = dplyr::row_number(),
+                tot_sims = tot_sims) %>%
+  dplyr::relocate(.data = ., tot_sims, sim_no, reps)
 
 # Unit test: the grid has correct number of rows
-length(exp_n) * length(exp_sigma) * length(exp_eta) * length(exp_p) *
-  length(exp_reps) - base::nrow(asci_grid)
+assertthat::assert_that(tot_sims - base::nrow(asci_grid) == 0)
 
 # Run ASCIFIT over the grid
 out_ascifit1 <- asci_grid %>%
-  dplyr::mutate(asci_data = purrr::pmap(.l = list(.data[["n"]],
-                                                  .data[["sigma"]],
-                                                  .data[["eta"]],
-                                                  .data[["p"]]), .f = gen_asci_data_type1),
-                ascifit = purrr::map2(.x = .data[["asci_data"]],
+  dplyr::mutate(asci_data_out = purrr::pmap(.l = list(.data[["tot_sims"]],
+                                                      .data[["sim_no"]],
+                                                      .data[["n"]],
+                                                      .data[["p"]],
+                                                      .data[["eta"]],
+                                                      .data[["sigma"]]),
+                                            .f = gen_asci_data_type1),
+                ascifit_out = purrr::map2(.x = .data[["asci_data_out"]],
                                       .y = .data[["eta"]],
                                       .f = ~ascifit_wrapper(gen_asci_data = .x,
-                                                            eta = .y)))
-out_ascifit2 <- out_ascifit1 %>%
-  dplyr::mutate(mu_hat_inv = purrr::map(.x = .data[["ascifit"]], .f = ~.x$mu_hat_inv),
-                sigma_hat = purrr::map(.x = .data[["ascifit"]], .f = ~.x$sigma_hat),
-                mu_hat = purrr::map(.x = .data[["ascifit"]], .f = ~.x$mu_hat),
-                mu = purrr::map(.x = .data[["asci_data"]], .f = ~.x$mu_i_eta),
-                mse = purrr::map2_dbl(.x = .data[["mu_hat"]], .y = .data[["mu"]],
-                                  .f = ~mse(estimate = .x, actual = .y)))
-out_ascifit2$mse
+                                                            eta = .y))) %>%
+  dplyr::mutate(ascifit_comb = purrr::pmap(.l = list(.data[["asci_data_out"]],
+                                                     .data[["ascifit_out"]]),
+                                           .f = asci_data_wrapper))
 
-# Plot the data
-out_ascifit2_mse <- out_ascifit2 %>%
-  dplyr::select(n, eta, p, sigma, mse) %>%
-  # dplyr::filter(eta == 1/5, p == 0.25, sigma == 1) %>%
-  dplyr::filter(eta == 1/5, p == 0.25) %>%
-  group_by(n, sigma) %>%
-  dplyr::summarize(mean_mse = base::mean(mse),
-                   sd_mse = stats::sd(mse)) %>%
-  dplyr::mutate(se_mse = sd_mse/base::sqrt(n),
-                sigma = as.factor(sigma),
-                n = as.factor(n))
-
-# The errorbars overlapped, so use position_dodge to move them horizontally
-pd <- ggplot2::position_dodge(0.1) # move them .05 to the left and right
-
-out_ascifit2_mse %>%
-ggplot2::ggplot(data = ., ggplot2::aes(x = n, y = mean_mse, color = sigma)) +
-  ggplot2::geom_errorbar(ggplot2::aes(ymin = mean_mse - 2*se_mse,
-                                      ymax = mean_mse + 2*se_mse), width = .1,
-                         position = pd) +
-  ggplot2::geom_line(position=pd) +
-  ggplot2::geom_point(position=pd, size=3, shape=21, fill="white") + # 21 is filled circle
-  ggplot2::labs(title = "Mean MSE for estimating mu using ASCIFIT",
-                x = "Sample size (n)",
-                y = latex2exp::TeX("Sample Mean-MSE $\\| \\hat{\\mu} - mu \\|^{2}$")) +
-  ggplot2::theme_bw() +
-  ggplot2::theme(legend.justification=c(1,0),
-        legend.position=c(1,0))               # Position legend in bottom right
-
-
-out_ascifit3 <- out_ascifit2 %>%
-  dplyr::filter(eta == 1/5, p == 0.25, sigma == 1, reps == 1, n == 200)
-
-out_ascifit3$asci_data
-out_ascifit3$ascifit
-
+# Write out the tibble to local directory
+readr::write_rds(x = out_ascifit1,
+                 file = here::here("experiments", "R", "out_ascifit1.rds"),
+                 compress = "xz", compression = 9L)
